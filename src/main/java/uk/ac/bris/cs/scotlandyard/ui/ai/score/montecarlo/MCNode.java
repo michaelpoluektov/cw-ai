140d914947974bda83d77f1d7f600c5d27997b33
michael_poluektov <michael.poluektov@gmail.com>
2021-05-06 16:59:24 +0200

    Skeleton compiles

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/score/montecarlo/MCNode.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/score/montecarlo/MCNode.java
@@ -9,7 +9,14 @@ public class MCNode {
     private final MiniBoard miniBoard;
     private Integer plays;
     private Integer score;
-    private final Optional<MiniBoard> parent;
-    private Optional<ImmutableSet<MiniBoard>> children;
+    private final Optional<MCNode> parent;
+    private Optional<ImmutableSet<MCNode>> children;
 
+    protected MCNode(MiniBoard miniBoard, MCNode parent) {
+        this.miniBoard = miniBoard;
+        this.plays = 0;
+        this.score = 0;
+        this.parent = Optional.of(parent);
+        this.children = Optional.empty();
+    }
 }
