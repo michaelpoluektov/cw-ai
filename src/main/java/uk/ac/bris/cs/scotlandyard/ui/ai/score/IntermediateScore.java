158c0fe581efec38467621083a7631470c96e33f
michael_poluektov <michael.poluektov@gmail.com>
2021-05-08 18:44:11 +0200

    Changed IntermediateScore comments for TODO

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/score/IntermediateScore.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/score/IntermediateScore.java
@@ -3,12 +3,18 @@ package uk.ac.bris.cs.scotlandyard.ui.ai.score;
 import javax.annotation.Nonnull;
 
 public interface IntermediateScore {
+
+    @Nonnull Double getScore();
     /*
     @param: MiniBoard to score
 
     Return: Score evaluating the GameState from the agent's perspective, from 0 (least favorable) to 1 (most favorable)
     */
-    @Nonnull Double getScore();
 
     @Nonnull Double getWeight();
+    /*
+    @param: Toml config file
+
+    Return: Weight of scoring function from Toml config
+    */
 }
