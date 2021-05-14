8bf9b042c75d12431d6f8dc40903218dccdf7727
michael_poluektov <michael.poluektov@gmail.com>
2021-05-14 07:31:32 +0200

    Moved class out of lambda + annotation

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/location/montecarlo/parallel/ParallelMonteCarlo.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/location/montecarlo/parallel/ParallelMonteCarlo.java
@@ -8,7 +8,6 @@ import uk.ac.bris.cs.scotlandyard.ui.ai.MiniBoard;
 import uk.ac.bris.cs.scotlandyard.ui.ai.location.LocationPicker;
 import uk.ac.bris.cs.scotlandyard.ui.ai.location.montecarlo.AbstractMonteCarlo;
 import uk.ac.bris.cs.scotlandyard.ui.ai.location.montecarlo.AbstractNode;
-import uk.ac.bris.cs.scotlandyard.ui.ai.location.montecarlo.standard.StandardNode;
 import uk.ac.bris.cs.scotlandyard.ui.ai.score.IntermediateScore;
 
 import javax.annotation.Nonnull;
@@ -17,19 +16,21 @@ import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * This class is used for the "smart" Monte Carlo algorithm which is the final Ai we have implemented. It uses
- * concepts from concurrent computing and parallelises the threads to maximise the number of simulations of the tree.
+ * This class is used for the "smart" Monte Carlo algorithm which is our latest AI implementation. It uses concurrent
+ * computing the threads to maximise the number of simulations of the tree.
  * Since the path a simulation takes down the tree is dependant on other simulations and the results they yield (number
- * of times we have played a node), we needed to backpropagate the number of plays before allowing another thread
- * to run a concurrent simulation. This was the reason back propagate plays and back propagate score were separated in
- * {@link AbstractNode}.
+ * of times we have played a node), the plays are backpropagated before allowing another thread to run a concurrent
+ * simulation. This justifies the separation of the backpropagation of plays and score.
+ * @see AbstractNode
  */
+
 
 public class ParallelMonteCarlo extends AbstractMonteCarlo implements LocationPicker {
     private final IntermediateScore[] intermediateScores;
     private final Integer explorationFrequency;
     private final ParallelRootNode rootNode;
     private final Integer coreNumber = Runtime.getRuntime().availableProcessors();
+    private final ReentrantLock lock = new ReentrantLock();
     public ParallelMonteCarlo(Board board,
                               Toml constants,
                               IntermediateScore... intermediateScores) {
@@ -39,22 +40,12 @@ public class ParallelMonteCarlo extends AbstractMonteCarlo implements LocationPi
     }
 
     /**
-     * We instantiate a new {@link ThreadPoolExecutor} which takes as parameter the number of cores the pc running the programme has. This
-     * dictates the maximum number of threads we should have running at one time. The {@link ArrayBlockingQueue} refers to the
-     * maximum number of runnables the queue can store. If we try to execute a runnable and there are no threads
-     * available in the {@link ThreadPoolExecutor} and the queue is full, then the CallerRunsPolicy is called (Last parameter).
-     * This uses the thread responsible for the execution of the while loop to execute this runnable therefore preventing any
-     * other runnable from entering the while loop.
+     * The method uses a {@link ThreadPoolExecutor} with the maximum available number of cores. It manages access to
+     * those threads with a very short queue and a {@link ThreadPoolExecutor.CallerRunsPolicy} which, which prevents the
+     * queue from overfilling and the method not returning in time.
      *
-     * <p>- In order to concurrently run threads we use the {@link ReentrantLock} which prevents threads altering
-     * variables that other threads will use at the same time. The first thread that reaches the while loop locks the
-     * lambda function until it has back propagated the plays, at which point it unlocks the lambda function allowing the
-     * next thread to enter. Once the current time exceeds the end tme we shutdiwn the executor and select the best child
-     * node</p>
-     *
-     * @param destinations ImmutableSet of all available destinations
-     * @param simulationTime Maximum time to be used to return a result
-     * @return
+     * @param simulationTime Maximum time to be used to return a result, it is recommended to allow for a delay of at
+     *                       least 1 second.
      */
     @Nonnull
     @Override
@@ -68,30 +59,11 @@ public class ParallelMonteCarlo extends AbstractMonteCarlo implements LocationPi
                 coreNumber,
                 0,
                 TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<>(1),
+                new ArrayBlockingQueue<>(coreNumber),
                 new ThreadPoolExecutor.CallerRunsPolicy());
-        final ReentrantLock lock = new ReentrantLock();
+        RunnableSimulation runnableSimulation = new RunnableSimulation();
         while(currentTime < endTime) {
-            executor.execute(() -> {
-                ParallelNode selectedNode;
-                lock.lock();
-                try {
-                    selectedNode = rootNode.selectNode();
-                    selectedNode.expand();
-                    if(!selectedNode.getChildren().isEmpty()) {
-                        selectedNode = (ParallelNode) selectedNode.getChildren().asList().get(0);
-                    } else {
-                        MiniBoard.winner winner = selectedNode.getMiniBoard().getWinner();
-                        if(winner == MiniBoard.winner.NONE) throw new RuntimeException("State with no children has no winner!");
-                    }
-                    selectedNode.backPropagatePlays();
-                } finally {
-                    lock.unlock();
-                }
-                Integer rolloutResult = selectedNode.rollout(intermediateScores);
-                selectedNode.backPropagateScore(rolloutResult, rootNode.getRound());
-
-            });
+            executor.execute(runnableSimulation);
             currentTime = System.currentTimeMillis();
             if(rootNode.getPlays() % explorationFrequency == 0) {
                 Double maxScore = Collections.max(rootNode.getChildren(),
@@ -112,6 +84,38 @@ public class ParallelMonteCarlo extends AbstractMonteCarlo implements LocationPi
                 + " with score "
                 + bestEntry.getValue());
         return bestEntry;
+    }
+
+    /**
+     * Runs a single iteration of the MCTS algorithm.
+     *
+     * In order to manage access to potentially non thread safe variables, we use a {@link ReentrantLock}
+     * which prevents threads from simultaneously altering those variables. This should not have any considerable impact
+     * on performance, since the most time intensive operation to run is outside of the lock.
+     */
+    private class RunnableSimulation implements Runnable {
+        @Override
+        public void run() {
+            ParallelNode selectedNode;
+            lock.lock();
+            try {
+                selectedNode = rootNode.selectNode();
+                selectedNode.expand();
+                if(!selectedNode.getChildren().isEmpty()) {
+                    selectedNode = (ParallelNode) selectedNode.getChildren().asList().get(0);
+                } else {
+                    MiniBoard.winner winner = selectedNode.getMiniBoard().getWinner();
+                    if(winner == MiniBoard.winner.NONE) {
+                        throw new RuntimeException("State with no children has no winner!");
+                    }
+                }
+                selectedNode.backPropagatePlays();
+            } finally {
+                lock.unlock();
+            }
+            Integer rolloutResult = selectedNode.rollout(intermediateScores);
+            selectedNode.backPropagateScore(rolloutResult, rootNode.getRound());
+        }
     }
 
     @Override public void addDestinations(ImmutableSet<Integer> destinations) {
