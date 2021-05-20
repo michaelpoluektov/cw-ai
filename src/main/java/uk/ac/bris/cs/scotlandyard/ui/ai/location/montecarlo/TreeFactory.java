562eb578036849223828de00808ed159bb5b1f57
michael_poluektov <michael.poluektov@gmail.com>
2021-05-20 16:29:43 +0200

    Format

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/location/montecarlo/TreeFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/location/montecarlo/TreeFactory.java
@@ -13,24 +13,22 @@ import uk.ac.bris.cs.scotlandyard.ui.ai.score.IntermediateScore;
  */
 public class TreeFactory {
 
-
     public static TreeSimulation newHeavyTree(Board board,
                                               Toml constants,
                                               Integer coreNumber,
                                               IntermediateScore... intermediateScores) {
         final String configPrefix = "monteCarlo.heavy.";
-        final Double expConstant = constants.getDouble(configPrefix+"explorationConstant", 3.0);
+        final Double expConstant = constants.getDouble(configPrefix+"explorationConstant");
         final NodeUCTComparator comparator = new NodeUCTComparator(expConstant);
         final HeavyNode rootNode = new HeavyNode(new MiniBoard(board), null, comparator);
         return new TreeSimulation(constants, rootNode, coreNumber, intermediateScores);
     }
-
-
+    
     public static TreeSimulation newLightTree(Board board,
                                               Toml constants,
                                               Integer coreNumber) {
         final String configPrefix = "monteCarlo.light.";
-        final Double expConstant = constants.getDouble(configPrefix+"explorationConstant", 3.0);
+        final Double expConstant = constants.getDouble(configPrefix+"explorationConstant");
         final NodeUCTComparator comparator = new NodeUCTComparator(expConstant);
         final LightNode rootNode = new LightNode(new MiniBoard(board), null, comparator);
         return new TreeSimulation(constants, rootNode, coreNumber);
